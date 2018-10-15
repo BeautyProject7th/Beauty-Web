55f9beff5aaba82f38199d013391cde8d57d8e0a
tmfdk333 <tmfdk333@gmail.com>
2018-10-16 05:48:40 +0900

    브랜드 삭제 추가

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -35,6 +35,12 @@ class BrandsController < ApplicationController
 		@brand = Brand.find(params[:id])
 	end
 
+	def destroy
+		@brand = Brand.find(params[:id])
+		@brand.destroy
+		redirect_to brands_path
+	end
+
 	private
 		def brand_params
 			params.require(:brand).permit(:brand, :crawler)
